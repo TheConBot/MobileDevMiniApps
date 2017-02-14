395bfb188270d995035b7f0640e61ff794897e2b
Connor Botts <connor.botts@gmail.com>
2017-02-13 22:23:27 -0500

    Multi-touch

---
+++ Assets/Scripts/MultiTouchManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using InControl;
+
+public class MultiTouchManager : MonoBehaviour {
+
+    public Text touchText;
+
+	private void Update()
+    {
+        touchText.text = TouchManager.TouchCount.ToString();
+    }
+}
