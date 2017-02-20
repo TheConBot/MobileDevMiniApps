9162c0da89572d2790616d2a3b620de688bd4f4d
Connor Botts <connor.botts@gmail.com>
2017-02-20 17:21:41 -0500

    GyroGame

--- Assets/Scripts/GyroManager.cs
+++ Assets/Scripts/GyroManager.cs
@@ -2,16 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class GyroManager : MonoBehaviour {
-
-    public Rigidbody2D circle;
-    public float speed = 10;
-    private SpriteRenderer sprite;
+    
+    [Header("Player Movement")]
+    private Rigidbody2D body;
+    public float speed = 0.1f;
+    [Header("Circles")]
+    public GameObject[] circles;
+    private bool inCircle;
+    [Header("Play Area")]
+    public Vector2 bounds;
+    [Header("UI")]
+    public Text scoreUI;
+    public Text timerUI;
+    private int _score;
+    private int score
+    {
+        get { return _score; }
+        set {
+            _score = Mathf.Clamp(value, 0, 9999999);
+            scoreUI.text = _score.ToString();
+        }
+    }
+    private float _timer = 60;
+    private float timer
+    {
+        get { return _timer; }
+        set {
+            _timer = Mathf.Clamp(value, 0, 60);
+            timerUI.text = _timer.ToString("F");
+        }
+    }
 
     private void Start()
     {
-        sprite = GetComponent<SpriteRenderer>();
+        body = GetComponent<Rigidbody2D>();
+        SpawnNewCircle();
+        StartCoroutine(SpawnTimer());
     }
 
     private void Update()
@@ -20,17 +50,60 @@ public class GyroManager : MonoBehaviour {
         {
             SceneManager.LoadScene("Title");
         }
-        circle.AddForce(new Vector2(Input.acceleration.x, Input.acceleration.y) * speed);
-        Debug.Log(Input.acceleration);
+        timer -= Time.deltaTime;
+        if (inCircle)
+        {
+            score += 1;
+        }
+        body.AddForce(new Vector2(Input.acceleration.x, Input.acceleration.y) * speed);
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        sprite.color = Color.green;
+        if (other.GetComponent<SpriteRenderer>() != null)
+        {
+            other.GetComponent<SpriteRenderer>().color = Color.green;
+            inCircle = true;
+        }
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
-        sprite.color = Color.red;
+        if (other.GetComponent<SpriteRenderer>() != null)
+        {
+            other.GetComponent<SpriteRenderer>().color = Color.red;
+            inCircle = false;
+        }
+    }
+
+    private int FindAvailableCircle()
+    {
+        for (int i = 0; i < circles.Length; i++)
+        {
+            if (!circles[i].activeSelf)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public void SpawnNewCircle()
+    {
+        if (timer == 0) return;
+        int circleIndex = FindAvailableCircle();
+        if (circleIndex == -1) return;
+        float x = Random.Range(-bounds.x, bounds.x);
+        float y = Random.Range(-bounds.y, bounds.y);
+        circles[circleIndex].SetActive(true);
+        circles[circleIndex].transform.position = new Vector2(x, y);
+    }
+
+    private IEnumerator SpawnTimer()
+    {
+        float time = Random.Range(0, 3);
+        yield return new WaitForSeconds(time);
+        SpawnNewCircle();
+        StartCoroutine(SpawnTimer());
     }
 }
---
+++ Assets/Scripts/ScaleCircle.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleCircle : MonoBehaviour {
+    [Range(0, 0.1f)]
+    public float shrinkSpeed;
+    private Vector3 startingScale;
+    private GyroManager manager;
+    private bool settingUp;
+
+    private void Awake()
+    {
+        manager = GameObject.Find("Player").GetComponent<GyroManager>();
+        startingScale = transform.localScale;
+        gameObject.SetActive(false);
+    }
+
+	private void OnEnable()
+    {
+        transform.localScale = startingScale;
+        StartCoroutine(ScaleDown());
+    }
+	
+	private IEnumerator ScaleDown()
+    {
+        while (true)
+        {
+            transform.localScale = new Vector2(transform.localScale.x - shrinkSpeed, transform.localScale.y - shrinkSpeed);
+            if(transform.localScale.x <= 0)
+            {
+                manager.SpawnNewCircle();
+                gameObject.SetActive(false);
+            }
+            yield return new WaitForFixedUpdate();
+        }
+    }
+}
